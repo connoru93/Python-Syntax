24c62eac8e961d39bc56ce236389faaca3835074
connoru93 <ccudelhoven@gmail.com>
2025-01-30 18:13:09 -0600

    Lesson 3 Python Lists

---
+++ Lesson 3 Python Lists/Task 1 Python List Transformation.py
@@ -0,0 +1,8 @@
+# Given list of grades
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+# Sort the grades in descending order
+sorted_grades = sorted(grades, reverse=True)
+
+# Print the sorted list
+print("Sorted Grades (Descending Order):", sorted_grades)
---
+++ Lesson 3 Python Lists/Task 2 Python List Transformation.py
@@ -0,0 +1,8 @@
+# Given list of grades
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+# Calculate the average grade
+average_grade = sum(grades) / len(grades)
+
+# Print the average grade
+print("Average Grade:", average_grade)
